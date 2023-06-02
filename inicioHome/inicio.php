6ae412ec303cc1d51be05ae2e74c7b907d96ab0b
PelaoMadness <92385703+FranciscoValenzuelaPaz@users.noreply.github.com>
2023-06-02 15:17:58 -0400

    Add files via upload

--- inicioHome/inicio.php
+++ inicioHome/inicio.php
@@ -1,77 +1,198 @@
+<style>
+    body {
+        background-color: #2A7C6F !important;
+    }
 
-    <style>
-        body {
-            background-color: #bfbfbf;
-        }
+    .imgnormalizada img {
+        height: 600px !important;
+        max-height: 600px !important;
+    }
 
-        .imgnormalizada img {
-            height: 600px !important;
-            max-height: 600px !important;
-        }
+    .carrusel {
+        width: 101.6% !important;
+        margin-left: -11px !important;
+    }
 
-        .carrusel {
-            width: 100% !important;
-     
-        }
-        .justificado{
-            text-align: justify !important;
-            line-height: 30px !important;
-        }
-    </style>
+    .justificado {
+        line-height: 30px !important;
+        font-family: 'Times New Roman', Times, serif;
+        color: aliceblue;
+        font-size: 20px !important;
+        text-align: justify;
+        text-align: center !important;
+    }
+
+    .centrado {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    .acerca {
+        position: relative;
+        display: inline-block;
+        text-align: center;
+        color: aliceblue;
+    }
+
+    .eliminarfondoimagen {
+        filter: brightness(1.1);
+        mix-blend-mode: multiply;
+        padding: 50px;
+    }
+
+    .marginLeft {
+        margin-left: 11px !important;
+        background-image: url(../images/fondotexto.png);
+        padding: 50px;
+    }
+
+    .marginLeft2 {
+        margin-left: -12px !important;
+    }
+
+    .fondo {
+        background-color: white !important;
+        color: #2A7C6F !important;
+    }
+
+    .fondo_valores {
+        background-image: url("../images/fondo_valores2.png")
+    }
+</style>
+<html>
+
+<body>
     <div class="container contenedor">
         <?php include("../Header-Footer/header2.php"); ?>
         <br>
+        <div class="row">
+            <div class="col justificado marginLeft fondo">
+                <br>
+                <h3>¡Bienvenidos a RentFacil!</h3><br>
+                <p style="text-align: justify;font-size:16px !important;">Tu partner confiable en el mundo de los equipos industriales y la construcción!
+                    Contamos con una amplia variedad de maquinarias en arriendo, especialmente diseñadas para satisfacer tus necesidades.
+                    Además, ofrecemos servicios de estacionamiento especializados para el transporte seguro de tus equipos pesados. Nuestro
+                    compromiso con la calidad nos distingue, entregando soluciones confiables para empresas, hogares y obras. ¡Confía en RentFacil
+                    para llevar tus proyectos al siguiente nivel! Descubre por qué somos la opción preferida de los expertos de la industria y logra
+                    el éxito en cada desafío. ¡Vamos juntos por el camino del éxito!</p>
+            </div>
+            <div class="col marginLeft2">
+                <img src="../images/Diferencias-entre-obra-pública-y-obra-civil-v2-3.jpg" alt="imagen" style="width: 100% !important;">
+            </div>
+        </div>
+        <br><br>
+        <hr>
+        <div class="justificado">
+            <h1>Maquinarias </h1>
+            <p>Arriendo de Maquinaria a excelentes Precios</p>
+        </div>
+        <br><br>
+        <div class="row">
+            <div class="col justificado marginLeft fondo">
+                <br>
+                <h3 style="text-align: center">Tractor</h3>
+                <br>
+                <p style="text-align: justify;font-size:20px !important;">¡Potencia tus proyectos agrícolas o de construcción con nuestro arriendo de tractores de última generación!
+                    En RentFacil, entendemos que tu tiempo y recursos son valiosos,
+                    por eso te ofrecemos la solución perfecta para tus necesidades de maquinaria!</p>
+            </div>
+            <div class="col marginLeft2">
+                <img src="../images/tractor.png" alt="imagen" style="width: 100% !important;">
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col justificado marginLeft fondo">
+                <br>
+                <h3 style="text-align: center">Hormigonera</h3>
+                <br>
+                <p style="text-align: justify;font-size:20px !important;">¡Ahorra tiempo y esfuerzo en tus proyectos de construcción con nuestro arriendo de camiones hormigoneras!
+                    En RentFacil, te ofrecemos la solución perfecta para tus necesidades de transporte y
+                    mezclado de hormigón!</p>
+            </div>
+            <div class="col marginLeft2">
+                <img src="../images/hormigonera.png" alt="imagen" style="width: 100% !important;">
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col justificado marginLeft fondo">
+                <br>
+                <h3 style="text-align: center">Compactadores de rodillos</h3>
+                <br>
+                <p style="text-align: justify;font-size:20px !important;">¡Optimiza tus proyectos de construcción con nuestro arriendo de compactadores de rodillos! En RentFacil,
+                    te ofrecemos la solución perfecta para lograr una compactación eficiente y de calidad!</p>
+            </div>
+            <div class="col marginLeft2">
+                <img src="../images/compactador.png" alt="imagen" style="width: 100% !important;">
+            </div>
+        </div>
+        <hr>
+        <div class="row justificado fondo_valores" style="width:1320px !important;margin-left: -12px !important;height: 600px !important;opacity: 0.6 !important;">
+
+            <div>
+                <h2 style="text-align: center; margin-top:20px !important;">Nuestros Valores</h2>
+                <br>
+                <p>Descubre RentFacil, donde la seguridad y el compromiso son nuestros valores fundamentales. Somos una empresa comprometida con brindar servicios de maquinarias de construcción de primera calidad, con un enfoque inquebrantable en la seguridad y el compromiso con nuestros clientes.
+
+                    En RentFacil la seguridad es nuestra prioridad número uno. Nos esforzamos por garantizar un
+                    entorno de trabajo seguro tanto para nuestro equipo como para nuestros clientes.
+                    Cumplimos con las normas más estrictas de seguridad industrial y promovemos prácticas seguras en cada
+                    aspecto de nuestras operaciones.
+                    Tu tranquilidad y bienestar son de suma importancia para nosotros.</p>
+                </p>
+            </div>
+            <div class="col eliminarfondoimagen " style="display: flex;margin-left:10% ">
+                <img src="../images/seguridad2.png" alt="imagen" style="width: 250px !important;">
+            </div>
+            <div class="col eliminarfondoimagen" style="display: flex; margin-left:25%; ">
+                <img src="../images/compromiso2.png " alt="imagen" style="width: 250px !important;">
+            </div>
+        </div>
+        <hr>
+        <div class="acerca justificado  ">
+            <img src="../images/acerca.png" style="width:1320px !important;margin-left: -12px !important;height: 600px !important;height: 600px !important;opacity: 0.6 !important;" />
+            <div class="centrado" style="text-align: justify !important;font-size:24px !important;">
+                ¡Descubre RentFacil y todo lo que tenemos para ofrecerte!
+                Somos un proveedor de servicios de maquinarias de construcción de diferentes tamaños en la industria.
+                Fundada en el año 2023, nos enorgullece ofrecer servicios y herramientas de excelente calidad con
+                los mejores precios disponibles en Santiago y más allá.
+                Nuestro objetivo es brindarte soluciones eficientes y confiables para tus proyectos de construcción.
+                Contamos con una amplia gama de maquinarias de construcción, desde excavadoras y retroexcavadoras hasta grúas
+                y compresores, para cubrir todas tus necesidades.</div>
+        </div>
+        <br><br>
         <div id="carouselExampleAutoplaying" class="carousel slide carrusel" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active imgnormalizada">
-                    <img src="../images/2-1F516103020964.jpg" class="d-block w-100" alt="imagen">
+                    <img src="../images/carrusel1.png" class="d-block w-100" alt="imagen">
                 </div>
                 <div class="carousel-item imgnormalizada">
-                    <img src="../images/foto3.png" class="d-block w-100" alt="imagen">
+                    <img src="../images/carrusel2.png" class="d-block w-100" alt="imagen">
+                </div>
+                <div class="carousel-item imgnormalizada">
+                    <img src="../images/carrusel3.png" class="d-block w-100" alt="imagen">
                 </div>
                 <div class="carousel-item imgnormalizada">
                     <img src="../images/generador-electrico-construccion-1.png" class="d-block w-100" alt="imagen">
                 </div>
-                <div class="carousel-item imgnormalizada">
-                    <img src="../images/sk-rental-1-896x504.png" class="d-block w-100" alt="imagen">
-                </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
-                data-bs-slide="prev">
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
-                data-bs-slide="next">
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
         </div>
-        <br><br><br>
-
-        <div class="row">
-            <div class="col">
-                <img src="../images/Diferencias-entre-obra-pública-y-obra-civil-v2-3.jpg" alt="imagen"
-                    style="width: 700px !important;">
-            </div>
-            <div class="col justificado">
-            ¡Bienvenidos a RentFacil, tu partner confiable en el mundo de los equipos industriales y la construcción! 
-            Contamos con una amplia variedad de maquinarias en arriendo, especialmente diseñadas para satisfacer tus necesidades. 
-            Además, ofrecemos servicios de estacionamiento especializados para el transporte seguro de tus equipos pesados. Nuestro 
-            compromiso con la calidad nos distingue, entregando soluciones confiables para empresas, hogares y obras. ¡Confía en RentFacil 
-            para llevar tus proyectos al siguiente nivel! Descubre por qué somos la opción preferida de los expertos de la industria y logra 
-            el éxito en cada desafío. ¡Vamos juntos por el camino del éxito!
-            </div>
-        </div>
-
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-            crossorigin="anonymous"></script>
-
         <br><br>
-        <?php //include("../Header-Footer/footer.php"); ?>
+        <?php include("../Header-Footer/footer.php"); ?>
     </div>
+    <br><br>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <br><br>
 </body>
 
