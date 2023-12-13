9dc75548553cd4d58ef70702e8a14eb82959d116
PelaoMadness <92385703+FranciscoValenzuelaPaz@users.noreply.github.com>
2023-12-12 23:31:07 -0400

    Add files via upload

--- inicioHome/inicio.php
+++ inicioHome/inicio.php
@@ -1,138 +1,47 @@
 <style>
-    <?php include("../CSS/inicio.css"); ?>
+    <?php include("../CSS/inicio.css"); ?><?php include("../Header-Footer/header2.php"); ?>
 </style>
 
 <html>
 
-<body class="body">
-    <div class="container contenedor">
-        <?php include("../Header-Footer/header2.php"); ?>
-        <div class="row margen">
-            <div class="col textoIzquierda">
-                <h3><strong>¡Bienvenidos a RentFacil!</strong></h3>
-                <span>Tu partner confiable en el mundo de los equipos industriales y la construcción!
-                    Contamos con una amplia variedad de maquinarias en arriendo, especialmente diseñadas para satisfacer tus necesidades.
-                    Además, ofrecemos servicios de estacionamiento especializados para el transporte seguro de tus equipos pesados. Nuestro
-                    compromiso con la calidad nos distingue, entregando soluciones confiables para empresas, hogares y obras. ¡Confía en RentFacil
-                    para llevar tus proyectos al siguiente nivel! Descubre por qué somos la opción preferida de los expertos de la industria y logra
-                    el éxito en cada desafío. ¡Vamos juntos por el camino del éxito!
-                </span>
-            </div>
-            <div class="col">
-                <img src="../images/Diferencias-entre-obra-pública-y-obra-civil-v2-3.jpg" class="imagenesDerecha" alt="imagen">
-            </div>
-        </div>
-        <div class="titulos margen">
-            <h1><strong>MAQUINARIAS</strong></h1>
-            <span><em>Arriendo de Maquinaria a excelentes Precios</em></span>
-        </div>
+<body>
+    <div class="container">
 
-        <div class="row margen">
-            <div class="col textoIzquierda">
-                <h3><strong>¡TRACTORES!</strong></h3>
-                <span>
-                    ¡Potencia tus proyectos agrícolas o de construcción con nuestro arriendo de tractores de última generación!
-                    En RentFacil, entendemos que tu tiempo y recursos son valiosos,
-                    por eso te ofrecemos la solución perfecta para tus necesidades de maquinaria!
-                </span>
-            </div>
-            <div class="col">
-                <img src="../images/Diferencias-entre-obra-pública-y-obra-civil-v2-3.jpg" class="imagenesDerecha" alt="imagen">
-            </div>
+        <div class="titulo">
+            <h1><strong>BIENVENIDO A RENTFACIL</strong></h1>
+            <span><em>
+                    "Ofrecemos servicios de gestión de arriendo de maquinaria y estacionamientos para el transporte,
+                    garantizando soluciones integrales y eficientes."
+                </em></span>
         </div>
-
-        <div class="row margen">
-            <div class="col textoIzquierda">
-                <h3><strong>¡HORMIGONERAS!</strong></h3>
-                <span>
-                    ¡Ahorra tiempo y esfuerzo en tus proyectos de construcción con nuestro arriendo de camiones hormigoneras!
-                    En RentFacil, te ofrecemos la solución perfecta para tus necesidades de transporte y
-                    mezclado de hormigón!
-                </span>
-            </div>
-            <div class="col">
-                <img src="../images/hormigonera.png" class="imagenesDerecha" alt="imagen">
-            </div>
-        </div>
-
-        <div class="row margen">
-            <div class="col textoIzquierda">
-                <h3><strong>¡COMPACTADORA DE RODILLOS!</strong></h3>
-                <span>
-                    ¡Optimiza tus proyectos de construcción con nuestro arriendo de compactadores de rodillos! En RentFacil,
-                    te ofrecemos la solución perfecta para lograr una compactación eficiente y de calidad!
-                </span>
-            </div>
-            <div class="col">
-                <img src="../images/compactador.png" class="imagenesDerecha" alt="imagen">
-            </div>
-        </div>
-        <div class="row fondo_valores margen">
-            <div class="texto_valores">
-                <h3><strong>NUESTROS VALORES</strong></h3>
-                <span>
-                    <em>
-                        "Descubre RentFacil, donde la seguridad y el compromiso son nuestros valores fundamentales.
-                        Somos una empresa comprometida con brindar servicios de maquinarias de construcción de primera calidad,
-                        con un enfoque inquebrantable en la seguridad y el compromiso con nuestros clientes.
-                        En RentFacil la seguridad es nuestra prioridad número uno. Nos esforzamos por garantizar un
-                        entorno de trabajo seguro tanto para nuestro equipo como para nuestros clientes.
-                        Cumplimos con las normas más estrictas de seguridad industrial y promovemos prácticas seguras en cada
-                        aspecto de nuestras operaciones.
-                        Tu tranquilidad y bienestar son de suma importancia para nosotros".
-                    </em>
-                </span>
-            </div>
-            <div class="col">
-                <img src="../images/seguridad2.png" class="img_valores" alt="imagen">
-            </div>
-            <div class="col">
-                <img src="../images/compromiso2.png " class="img_valores" alt="imagen">
-            </div>
-        </div>
-        <div class="acerca">
-            <img class="img_acerca" src="../images/acerca.png" />
-            <div>
-                <span class="texto_acerca"><b>
-                        ¡Descubre RentFacil y todo lo que tenemos para ofrecerte!
-                        Somos un proveedor de servicios de maquinarias de construcción de diferentes tamaños en la industria.
-                        Fundada en el año 2023, nos enorgullece ofrecer servicios y herramientas de excelente calidad con
-                        los mejores precios disponibles en Santiago y más allá.
-                        Nuestro objetivo es brindarte soluciones eficientes y confiables para tus proyectos de construcción.
-                        Contamos con una amplia gama de maquinarias de construcción, desde excavadoras y retroexcavadoras hasta grúas
-                        y compresores, para cubrir todas tus necesidades.</b>
-
-                </span>
-            </div>
-        </div>
-        <div id="carouselExampleAutoplaying" class="carousel slide carrusel margen" data-bs-ride="carousel">
-            <div class="carousel-inner carrusel">
-                <div class="carousel-item active imgnormalizada">
-                    <img src="../images/carrusel1.png" class="carrusel" alt="imagen">
+        <div id="carouselExampleFade" class="carousel slide carousel-fade mb-5 mt-5" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="../images/Maquinarias_inicio.png" class="img d-block w-100" alt="...">
                 </div>
-                <div class="carousel-item imgnormalizada">
-                    <img src="../images/carrusel2.png" class="carrusel" alt="imagen">
-                </div>
-                <div class="carousel-item imgnormalizada">
-                    <img src="../images/carrusel3.png" class="carrusel" alt="imagen">
-                </div>
-                <div class="carousel-item imgnormalizada">
-                    <img src="../images/generador-electrico-construccion-1.png" class="carrusel" alt="imagen">
+                <div class="carousel-item">
+                    <img src="../images/Maquinarias2_inicio.png" class="img d-block w-100" alt="...">
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
         </div>
-        <?php include("../Header-Footer/footer.php"); ?>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
 </body>
+<footer>
+    <?php include("../Header-Footer/footer.php"); ?>
+</footer>
 
 </html>
