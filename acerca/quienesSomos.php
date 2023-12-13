a77a8eb09ca5bb0be7c33b71a3953ae0e069076a
PelaoMadness <92385703+FranciscoValenzuelaPaz@users.noreply.github.com>
2023-12-12 23:27:19 -0400

    Add files via upload

---
+++ acerca/quienesSomos.php
@@ -0,0 +1,121 @@
+<style>
+    <?php include("../CSS/quienesSomos.css"); ?><?php include("../Header-Footer/header2.php"); ?>
+</style>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rentfacil</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="container">
+            <h2 class="titulo">ACERCA DE RENTFACIL </h2>
+            <div class="row">
+                <div class="col-md-4 texto_acerca">
+                    <div class="custom-column">
+                        <div class="form-group">
+                            <span><b>
+                                    ¡Descubre RentFacil y todo lo que tenemos para ofrecerte!
+                                    Somos un proveedor de servicios de maquinarias de construcción de diferentes tamaños en la industria.
+                                    Fundada en el año 2023, nos enorgullece ofrecer servicios y herramientas de excelente calidad con
+                                    los mejores precios disponibles en Santiago y más allá.
+                                    Nuestro objetivo es brindarte soluciones eficientes y confiables para tus proyectos de construcción.
+                                    Contamos con una amplia gama de maquinarias de construcción, desde excavadoras y retroexcavadoras hasta grúas
+                                    y compresores, para cubrir todas tus necesidades.</b>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="custom-column">
+                        <div class="form-group">
+                            <img src="../images/acerca.png" class="img_acerca" alt="Imagen Acerca">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="border mt-5 mb-5">
+            <h2 class="titulo">NUESTRA HISTORIA</h2>
+            <div class="texto_acerca mb-5">
+                <p>Nuestra empresa se fundó en el año 2023 y desde entonces, nos hemos esforzado por ser líderes en la industria de alquiler de maquinaria de construcción y servicios de estacionamiento para vehiculos de trasporte de maquinarias.</p>
+            </div>
+        </div>
+
+        <div class="container">
+            <h2 class="titulo">NUESTROS VALORES</h2>
+            <div class="row fondo_valores">
+                <div class="col-md-6">
+                    <div class="texto_valores">
+                        <span>
+                            <em>
+                                "Descubre RentFacil, donde la seguridad y el compromiso son nuestros valores fundamentales.
+                                Somos una empresa comprometida con brindar servicios de maquinarias de construcción de primera calidad,
+                                con un enfoque inquebrantable en la seguridad y el compromiso con nuestros clientes.
+                                En RentFacil la seguridad es nuestra prioridad número uno. Nos esforzamos por garantizar un
+                                entorno de trabajo seguro tanto para nuestro equipo como para nuestros clientes.
+                                Tu tranquilidad y bienestar son de suma importancia para nosotros."
+                            </em>
+                        </span>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <img src="../images/seguridad2.png" class="img_valores" alt="Imagen Seguridad">
+                </div>
+                <div class="col-md-3">
+                    <img src="../images/compromiso2.png" class="img_valores" alt="Imagen Compromiso">
+                </div>
+            </div>
+        </div>
+
+
+
+        <h2 class="titulo">MISIÓN Y VISIÓN DE RENTFACIL</h2>
+        <div class="mision-vision border">
+            <div class="mision">
+                <h3>MISIÓN</h3>
+                <p>
+                    Nuestra misión es brindar las mejores soluciones en maquinaria para la construcción con calidad y seguridad garantizadas. Además, ofrecemos servicios de arrendamiento de estacionamiento exclusivos para vehículos de transporte de maquinaria. ¡Construimos tu proyecto, desde el equipo hasta su resguardo!</p>
+            </div>
+            <div class="vision">
+                <h3>VISIÓN</h3>
+                <p>
+                    Buscamos liderar el alquiler de maquinaria de construcción a nivel nacional, contribuyendo al éxito de nuestros clientes. También ofrecemos servicios de arrendamiento de estacionamiento para vehículos de transporte de maquinaria. ¡Tu proyecto, nuestra excelencia!</p>
+            </div>
+        </div>
+
+        <h2 class="titulo">NUESTRO EQUIPO</h2>
+        <div class="equipo-miembros mb-5 border-bottom">
+            <div class="miembro">
+                <img src="#" alt="Miembro 1">
+                <h3>Francisco Valenzuela</h3>
+                <p>Estudiante</p>
+            </div>
+            <div class="miembro">
+                <img src="#" alt="Miembro 2">
+                <h3>Gustavo Riquelme</h3>
+                <p>Estudiante</p>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+</body>
+
+<!-- Optional JavaScript; choose one of the two! -->
+<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+
+<!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+      -->
+<footer>
+    <?php include("../Header-Footer/footer.php"); ?>
+</footer>
+
+</html>
